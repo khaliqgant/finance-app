c4320d8f68426da4f15581219827a08cbf0dfb22
Khaliq Gant <khaliqgant@gmail.com>
2016-01-05 21:37:29 +0100

    add in stats logic and abstract out some of the agg logic

--- elasticsearch/query.py
+++ elasticsearch/query.py
@@ -1,7 +1,4 @@
-import logging
 import json
-import os
-
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 from elasticsearch_dsl.connections import connections
@@ -18,7 +15,21 @@ def run_aggregations():
 
     structure = files.cc_structure()
 
-    # store averages in a dict
+    average_agg(s, structure)
+    stats_agg(s, structure)
+
+
+
+def make_file(data, file_name):
+    # write this json to a file now
+    path = "../data/analysis/"
+    write_file = path + file_name
+    with open(write_file, 'w') as output_file:
+        json.dump(data, output_file)
+        print "aggregations written to ", file_name
+
+
+def average_agg(s, structure):
     averages = {}
     for card_type, arr in structure.iteritems():
         averages[card_type] = {}
@@ -31,15 +42,24 @@ def run_aggregations():
                 averages[card_type][card] = "%.2f" % average
             else:
                 print('aggregations failed %s', response.to_dict())
+    make_file(averages, "averages.json")
 
-    # write this json to a file now
-    path = "../data/analysis/"
-    if not os.path.exists(path):
-        os.makedirs(path)
-    averages_file = path + "averages.json"
-    with open(averages_file, 'w') as output_file:
-        json.dump(averages, output_file)
-        print "aggregations written to ", averages_file
+def stats_agg(s, structure):
+    stats = {}
+    for card_type, arr in structure.iteritems():
+        stats[card_type] = {}
+        for card, amount in arr.iteritems():
+            stats[card_type][card] = {}
+            s.aggs.bucket('cc_stats', 'nested', path=card_type)\
+                .metric('stats', 'stats', field=card_type + "." + card)
+            response = s.execute()
+            if response.success():
+                # round off the stats value
+                for key, value in response.aggregations.cc_stats.stats.to_dict().iteritems():
+                    stats[card_type][card][key] = "%.2f" % value
+            else:
+                print('aggregations failed %s', response.to_dict())
+    make_file(stats, "stats.json")
 
 
 if __name__ == '__main__':
