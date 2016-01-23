a96df320c93e4d348f087c0cde83d7825c7c10a3
Khaliq Gant <khaliqgant@gmail.com>
2016-01-23 02:13:57 -0500

    add in file move and sed command to swap dates in the files

--- date_change.sh
+++ date_change.sh
@@ -11,4 +11,33 @@ do
     mv $file data/$year$under$month.json
 done
 
-#elasticsearch --config=/usr/local/opt/elasticsearch/config/elasticsearch.yml
+DATAS=(
+    'cash'
+    'closing_dates'
+    'due_dates'
+    'links'
+    'notes'
+)
+
+for data in "${DATAS[@]}"
+do
+    files=$(find data/$data -type f -name "[0-9][0-9]_[0-9][0-9][0-9][0-9].json" -depth 1)
+    for file in $files
+    do
+        parsed="${file#data/$data/*}"
+        month="${parsed%%_*}"
+        year_only=${parsed%%.json*}
+        year="${year_only#*_}"
+        mv $file data/$data/$year$under$month.json
+    done
+done
+
+for i in 1 2 3 4 5
+do
+    grep -rl "[0-9][0-9]_[0-9][0-9][0-9][0-9]" data | xargs sed -i.bak 's/^\(.*\)\([a-z_]*\/\)\([0-9][0-9]\)\(\_\)\([0-9]*\)\(\.[a-z]*\)\(.*\)/\1\2\5\4\3\6\7/g'
+    rm data/*.bak
+done
+
+#cron in mac
+#https://discussions.apple.com/thread/5750011?tstart=0
+# http://stackoverflow.com/questions/19460102/sed-switch-the-order-of-first-and-last-word
