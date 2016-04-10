ad7c5f93e3a71f03303d3d6a36101151e1e068a3
Khaliq Gant <khaliqgant@gmail.com>
2016-04-10 00:16:08 -0400

    set title in index of routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Khaliq\'s Finances' });
 });
 
 module.exports = router;
